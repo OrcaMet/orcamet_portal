b1b87571b37768185729510182a99bbf5a6b616e
OrcaMet <stevecar55.sc@gmail.com>
2026-02-28 20:47:49 +0000

    Delete orcamet_portal/static/orcamet_portal/css/wsgi.py

--- orcamet_portal/static/orcamet_portal/css/wsgi.py
+++
@@ -1,7 +0,0 @@
-"""WSGI config for OrcaMet Portal."""
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orcamet_portal.settings")
-application = get_wsgi_application()
