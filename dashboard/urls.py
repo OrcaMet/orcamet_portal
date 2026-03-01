a1f3130546970fec65dc19a9c3f2f33f11241b42
OrcaMet <stevecar55.sc@gmail.com>
2026-03-01 07:24:57 +0000

    Add site detail and chart data URLs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,6 @@ app_name = "dashboard"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("site/<int:site_id>/", views.site_detail, name="site_detail"),
+    path("site/<int:site_id>/chart-data/", views.forecast_chart_data, name="chart_data"),
 ]
