9c205a6331c3ac6d38c7e918d84ee4d3f9f603fc
OrcaMet <stevecar55.sc@gmail.com>
2026-03-02 19:00:22 +0000

    Update __init__.py

--- forecasts/engine/__init__.py
+++ forecasts/engine/__init__.py
@@ -1,2 +1,5 @@
-from .runner import run_forecast_for_site, run_forecasts_all_active  # noqa
-from .map_interpolation import generate_uk_risk_map, generate_map_from_grid_run  # noqa
+from .runner import run_forecast_for_site, 
+run_forecasts_all_active  # noqa
+from .map_interpolation import generate_uk_risk_map, 
+generate_map_from_grid_run  # noqa
+
